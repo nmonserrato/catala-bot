85d447ce5d8ee001cbc6328abcd0731f0b973e6d
neeno <neeno@nuc.dev>
2020-08-19 20:10:00 +0200

    fix text nullable

--- src/main/kotlin/dev/neeno/catalabot/rest/Update.kt
+++ src/main/kotlin/dev/neeno/catalabot/rest/Update.kt
@@ -2,6 +2,6 @@ package dev.neeno.catalabot.rest
 
 data class Update(val message: Message)
 
-data class Message (val text: String, val chat: Chat)
+data class Message (val text: String?, val chat: Chat)
 
 data class Chat(val id: String)
